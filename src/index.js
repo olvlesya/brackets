52fd60156f8fdc2f456b4dc7c06da225f0f47824
Olesia Nikonova <lesyonoknou@gmail.com>
2020-03-21 16:51:22 +0100

    brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  const bracketsMap = new Map(bracketsConfig);
+  const bracketsStack = [];
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+    if (bracketsMap.has(current)) {
+      const currentClosingBracket = bracketsMap.get(current);
+      if (
+        // same open and close
+        currentClosingBracket === current &&
+        bracketsStack[bracketsStack.length - 1] === current
+      ) {
+        bracketsStack.pop();
+      } else {
+        bracketsStack.push(current);
+      }
+    } else {
+      const lastBracket = bracketsStack.pop();
+      const closingBracket = bracketsMap.get(lastBracket);
+      if (closingBracket !== current) {
+        return false;
+      }
+    }
+  }
+  return bracketsStack.length === 0;
+};
